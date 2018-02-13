2dfe5acfc1e7b7f8266d486464d9cc94e6fcf3cf
Andrey Korin <a.korin@outlook.com>
2018-02-13 19:07:35 +0300

    Added Timeago.js and Clipboard.js

--- frontend/src/pages/ViewPage.js
+++ frontend/src/pages/ViewPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Clipboard from 'clipboard';
+import TimeAgo from 'react-timeago';
 
 import { getBackendURL } from '../utils/config';
 import endpointService from '../services/endpoint';
@@ -14,12 +16,15 @@ class ViewPage extends React.Component {
 
     this.state = {
       endpointURL: `${getBackendURL()}/${params.uniqueId}`,
+      endpointUniqueId: params.uniqueId,
       loading: true,
       requests: []
     };
   }
 
   async componentDidMount() {
+    this.clipboard = new Clipboard(this.copyButton, { target: () => this.copyInput });
+
     let { match: { params } } = this.props,
         requests;
 
@@ -39,18 +44,22 @@ class ViewPage extends React.Component {
     });
   }
 
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
   render() {
-    let { endpointURL, requests, error } = this.state;
+    let { endpointURL, requests, error, endpointUniqueId } = this.state;
 
     return (
       <main className="App-ViewPage">
         <div className="container-fluid">
-          <div className="row justify-content-md-center mt-4">
-            <div className="col col-md-6 text-center">
+          <div className="row justify-content-center mt-4">
+            <div className="col col-sm-8 align-self-center text-center">
               <div className="input-group">
-                <input className="App-ViewPage-url-input form-control form-control-lg text-center" type="text" type="text" size="50" readOnly value={endpointURL} onClick={e => e.target.setSelectionRange(0, e.target.value.length)} />
+                <input className="App-ViewPage-url-input form-control form-control-lg text-center" type="text" type="text" size="50" readOnly value={endpointURL} ref={el => this.copyInput = el} />
                 <div className="input-group-append">
-                  <button className="btn btn-secondary" type="button">Copy</button>
+                  <button className="btn btn-secondary" type="button" ref={el => this.copyButton = el}>Copy</button>
                 </div>
               </div>
             </div>
@@ -61,16 +70,16 @@ class ViewPage extends React.Component {
           {requests.length > 0 && (
             <div className="App-ViewPage-RequestContainer list-group mt-4">
               {requests.map((request, idx) => (
-                <div key={idx} className="list-group-item list-group-item-action flex-column align-items-start">
-                  <strong>{request.method} /</strong><br />
-                  <span className="text-muted">{request.createdAt}</span>
+                <div key={idx} className="App-ViewPage-RequestContainer-item list-group-item list-group-item-action flex-column align-items-start">
+                  <strong>{request.method} <span className="text-muted">/{endpointUniqueId}</span></strong><br />
+                  <span className="text-muted"><TimeAgo date={request.createdAt} /></span>
                 </div>
               ))}
             </div>
           )}
 
           {!requests.length && (
-            <div className="App-ViewPage-RequestContainer-empty row justify-content-md-center mt-3">
+            <div className="App-ViewPage-RequestContainer-empty row justify-content-center mt-3">
               <div className="col col-md-6 align-self-center text-center">
                 <p>Requests will appear here</p>
               </div>
